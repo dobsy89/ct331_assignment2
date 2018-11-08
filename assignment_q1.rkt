92267a2461479812b49eca82c620f8c0b3ee0535
dobsy89 <d.obrien2@nuigalway.ie>
2018-11-08 16:40:08 +0000

    answered question 1

--- assignment_q1.rkt
+++ assignment_q1.rkt
@@ -1,1 +1,15 @@
 #lang racket
+;part i
+>(cons 2 3)
+
+;part ii
+>(cons 2 (cons 3(cons 4 empty)))
+
+;part iii
+>(cons "Davin" (cons 1 (cons (cons 2 (cons 3 (cons 4 empty))) '())))
+
+;part 4
+>(list "Davin" '1 (list 2 3 4 ))
+
+;part v
+>(append '("Davin") '(1) '((3 4 5)))
